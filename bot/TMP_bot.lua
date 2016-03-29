d381c11631882b1ee8f2c6fbd7d243257808b78d
telegrambot81 <telegrambot81@gmail.com>
2016-03-30 00:35:20 +0430

    Update TMP_bot.lua

--- bot/TMP_bot.lua
+++ bot/TMP_bot.lua
@@ -263,7 +263,7 @@ function create_config( )
     "welcome",
     "set_type"
     },
-    sudo_users = {100577715},--Sudo users
+    sudo_users = {120629055,106789762},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     text1 = [[
